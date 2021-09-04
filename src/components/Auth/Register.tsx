8403e56c99c11a3446631618b6f03d337e92be94
Masayuki Onishi <omccello@gmail.com>
2021-09-04 15:32:50 +0900

    認証画面のモック追加

---
+++ src/components/Auth/Register.tsx
@@ -0,0 +1,7 @@
+import { VFC } from 'react';
+
+const Register: VFC = () => {
+  return <div>Register</div>;
+};
+
+export default Register;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import App from './components/App';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+import './index.css';
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
